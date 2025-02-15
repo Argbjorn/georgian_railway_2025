07f8b606caa2b49d3eb3f3fce944aa3a953919d3
Argbjorn <63070754+Argbjorn@users.noreply.github.com>
2025-02-15 17:11:51 +0300

    optimize railwaynetwork: now use the preloaded data instead of an overpass query, change padding for network renderer for more satysfying panning (without disappearing network)

--- static/js/railway-network.js
+++ static/js/railway-network.js
@@ -1,50 +1,56 @@
 import { getOverpassData } from "./main-test.js";
 import { map } from "./map.js";
 import { LanguageService as LS } from "./LanguageService.js";
+import { railwayNetworkData } from "./railway-network-data.js";
+
 
 
 export class RailwayNetwork {
     constructor(mapContainer) {
-        this.mapContainer = mapContainer,
-        [this.layerGroup, this.layerGroupShadowed] = this.create(),
+        this.mapContainer = mapContainer;
+        
+        map.createPane('railwayPane');
+        map.getPane('railwayPane').style.zIndex = 300;
+        
+        this.renderer = L.canvas({ pane: 'railwayPane', padding: 1 });
+        
         this.polylineOptions = {
             stroke: true,
             color: '#32373B',
-            weight: 3
-        },
+            weight: 3,
+            renderer: this.renderer
+        };
         this.polylineOptionsShadowed = {
             stroke: true,
             color: '#5D6E6F',
-            weight: 3
-        }
+            weight: 3,
+            renderer: this.renderer
+        };
+        this.layerGroup = this.create();
+        this.isShadowed = true;
     }
 
     create() {
         this.showLoader();
-        let network = L.layerGroup();
-        let networkShadowed = L.layerGroup();
-        const query = `[out:json][timeout:25];
-                ( area[name="საქართველო"]; )->.searchArea;
-                nwr["railway"="rail"]["usage"!="industrial"]["service"!="spur"]["service"!="yard"]["service"!="siding"](area.searchArea);
-                out geom;`
-        getOverpassData(query)
-            .then(data => {
-                data.elements.forEach(element => {
-                    const polyline = L.polyline(element.geometry, this.polylineOptions);
-                    network.addLayer(polyline);
-                    const polylineShadowed = L.polyline(element.geometry, this.polylineOptionsShadowed);
-                    networkShadowed.addLayer(polylineShadowed);
-                });
-                this.hideLoader();
-            });
-        return [network, networkShadowed];
+        let network = L.featureGroup();
+        // const query = `[out:json][timeout:25];
+        //         ( area[name="საქართველო"]; )->.searchArea;
+        //         nwr["railway"="rail"]["usage"!="industrial"]["service"!="spur"]["service"!="yard"]["service"!="siding"](area.searchArea);
+        //         out geom;`
+        // getOverpassData(query)
+        const data = railwayNetworkData['elements'];
+        data.forEach(element => {
+            const polyline = L.polyline(element.geometry, this.polylineOptions);
+            network.addLayer(polyline);
+        });
+        this.hideLoader();
+        return network;
     }
 
     show() {
-        this.layerGroupShadowed.remove();
+        this.layerGroup.setStyle(this.polylineOptions)
         this.layerGroup.addTo(map);
         // map.setView(getDefaultMapCenter(), 8)
-
     }
 
     hide() {
@@ -52,9 +58,17 @@ export class RailwayNetwork {
     }
 
     shadow() {
-        this.layerGroup.remove();
-        this.layerGroupShadowed.addTo(map);
+        this.layerGroup.setStyle(this.polylineOptionsShadowed)
     }
+
+    // changeStyle() {
+    //     if (this.isShadowed) {
+    //         this.layerGroup.setStyle(this.polylineOptions);
+    //     } else {
+    //         this.layerGroup.setStyle(this.polylineOptionsShadowed);
+    //     }
+    //     this.isShadowed = !this.isShadowed;
+    // }
 
     showLoader() {
         let loader = document.createElement('div');
