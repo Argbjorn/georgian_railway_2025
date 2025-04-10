36d5070e347f2889d1b72b39bc119d8c3afd734c
Argbjorn <63070754+Argbjorn@users.noreply.github.com>
2025-04-10 20:45:11 +0300

    change tile servers

--- static/js/map.js
+++ static/js/map.js
@@ -1,15 +1,11 @@
 // Tile providers
 var atlasTiles = L.tileLayer('https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=fe84be0c72b64292b7e12b685142997a', {
     maxZoom: 19,
-    attribution: 'Tiles &copy; <a href="https://www.thunderforest.com/" target="_blank">Thunderforest</a> | Data &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap contributors</a>'
+    attribution: 'Tiles &copy; <a href="https://www.thunderforest.com/" target="_blank">Thunderforest ❤️</a> | Data &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap contributors</a>'
 });
 var osmTiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap contributors</a>'
-});
-var stadiaTiles = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png?api_key=ab75ce18-0db1-4383-b7d5-a474e0b0a818', {
-    maxZoom: 20,
-    attribution: '&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>'
 });
 
 // Initialize the map
@@ -23,8 +19,7 @@ export const map = L.map('map', {
 // Tile object for L.control
 const tiles = {
     "Standard OSM": osmTiles,
-    "Atlas": atlasTiles,
-    "Stadia": stadiaTiles
+    "Atlas": atlasTiles
 };
 const overlays = {}
 
