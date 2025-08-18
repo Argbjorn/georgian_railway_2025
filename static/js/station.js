00272a37bd2268b56f753a0f3d2f6b955c7b8dff
Argbjorn <63070754+Argbjorn@users.noreply.github.com>
2025-08-18 17:53:06 +0300

    station marker popup now has user language

--- static/js/station.js
+++ static/js/station.js
@@ -1,6 +1,7 @@
 import stateManager from "./state/mapStateManager.js";
 import { ANIMATION_DURATION } from "./constants.js";
 import { getBoundsPadding } from "./utils.js";
+import { LanguageService } from "./languageService.js";
 
 const stationIcons = {
   beach: `<svg class="station-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-tree-palm-icon lucide-tree-palm"><path d="M13 8c0-2.76-2.46-5-5.5-5S2 5.24 2 8h2l1-1 1 1h4"/><path d="M13 7.14A5.82 5.82 0 0 1 16.5 6c3.04 0 5.5 2.24 5.5 5h-3l-1-1-1 1h-3"/><path d="M5.89 9.71c-2.15 2.15-2.3 5.47-.35 7.43l4.24-4.25.7-.7.71-.71 2.12-2.12c-1.95-1.96-5.27-1.8-7.42.35"/><path d="M11 15.5c.5 2.5-.17 4.5-1 6.5h4c2-5.5-.5-12-1-14"/></svg>`,
@@ -43,6 +44,7 @@ export class Station {
       stateManager.selectStation(this);
     });
     this.marker.addTo(this.map);
+    const stationName = this[`name_${LanguageService.getCurrentLanguage()}`] || this.name_en;
     const popup = new maplibregl.Popup({
       offset: 10,
       closeButton: false,
@@ -50,7 +52,7 @@ export class Station {
       closeOnEscape: false,
     })
       .setLngLat([this.coords[1], this.coords[0]])
-      .setHTML(`<div>${this.name_en}</div>`);
+      .setHTML(`<div>${stationName}</div>`);
     this.marker.setPopup(popup);
 
     markerEl.addEventListener("mouseenter", () => {
